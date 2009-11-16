41c05708166bce66561eb02eea77861a3549485a
Sven Fuchs <svenfuchs@artweb-design.de>
2009-11-16 09:38:45 +0100

    update init.rb to only require globalize/active_record

--- init.rb
+++ init.rb
@@ -1,8 +1,1 @@
-require 'globalize/model/active_record'
-
-I18n.backend = Globalize::Backend::Static.new
-
-I18n.load_path = Globalize::LoadPath.new I18n.load_path
-I18n.load_path << "#{File.dirname(__FILE__)}/lib/locale"
-I18n.load_path << "#{RAILS_ROOT}/lib/locale"
-
+require 'globalize/active_record'
