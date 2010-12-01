2ce0f694bedd528a49dbf1f00218d4cda5115b9b
Tomasz Stachewicz <tomekrs@o2.pl>
2010-12-01 13:32:36 +0100

    fixed tests: now compatible with rails 3.0.3 way of reporting missing translations (dot instead of comma space as separator)

--- test/i18n/missing_translations_test.rb
+++ test/i18n/missing_translations_test.rb
@@ -25,11 +25,11 @@ class LogMissingTranslationsTest < Test::Unit::TestCase
 
   test "still returns the exception message for MissingTranslationData exceptions" do
     result = I18n.send(:missing_translations_log_handler, @exception, @locale, @key, @options)
-    assert_equal 'translation missing: en, foo', result
+    assert_match /translation missing: en(\W+)foo/, result
   end
   
   test "logs the missing translation to I18n.missing_translations_logger" do
     I18n.send(:missing_translations_log_handler, @exception, @locale, @key, @options)
-    assert_equal 'translation missing: en, foo', @logger
+    assert_match /translation missing: en(\W+)foo/, @logger
   end
 end
