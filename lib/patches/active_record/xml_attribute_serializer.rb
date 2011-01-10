42ee732c8d7881b53770b106cda335ae9aa54f12
Jeff Wallace <jeff@evolvingweb.ca>
2011-01-11 00:59:40 +0800

    Use klass.translates? per clemens suggestion.

--- lib/patches/active_record/xml_attribute_serializer.rb
+++ lib/patches/active_record/xml_attribute_serializer.rb
@@ -3,8 +3,7 @@ require 'active_record/serializers/xml_serializer'
 ActiveRecord::XmlSerializer::Attribute.class_eval do
   def compute_type_with_translations
     klass = @serializable.class
-    if klass.respond_to?(:translated_attribute_names) &&
-       klass.translated_attribute_names.include?(name.to_sym)
+    if klass.translates? && klass.translated_attribute_names.include?(name.to_sym)
       :string
     else
       compute_type_without_translations
