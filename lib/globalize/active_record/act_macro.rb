fe03ded9ba38fbd1571c54244034f4c8c67f4cd6
Jeff Wallace <jeff@tjwallace.ca>
2011-01-16 21:56:11 -0500

    Memoize ActMacro#class_name.

--- lib/globalize/active_record/act_macro.rb
+++ lib/globalize/active_record/act_macro.rb
@@ -25,8 +25,10 @@ module Globalize
       end
 
       def class_name
-        class_name = table_name[table_name_prefix.length..-(table_name_suffix.length + 1)].downcase.camelize
-        pluralize_table_names ? class_name.singularize : class_name
+        @class_name ||= begin
+          class_name = table_name[table_name_prefix.length..-(table_name_suffix.length + 1)].downcase.camelize
+          pluralize_table_names ? class_name.singularize : class_name
+        end
       end
 
       def translates?
