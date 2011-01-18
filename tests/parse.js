ca3af93f0fc284f09ec71b682337a16e488b96c0
jzaefferer <joern.zaefferer@gmail.com>
2011-01-18 11:48:27 +0100

    Integrated parseCurrency into parseFloat, dropping parseCurrency. Added lots of tests by iterating over all cultures, formatting and parsing each.

---
+++ tests/parse.js
@@ -0,0 +1,33 @@
+module("parseFloat");
+
+test("basics, float", function() {
+	equal( $.global.parseInt("5.51"), 5 );
+	equal( $.global.parseFloat("5.51"), 5.51 );
+	equal( $.global.parseInt("5,51", 10, "de-DE"), 5 );
+	equal( $.global.parseFloat("5,51", 10, "de-DE"), 5.51 );
+});
+
+test("format-parse all, float", function() {
+	$.each($.global.cultures, function(culture, cultureInfo) {
+		// TODO is there a better approach to verify cultures with no/just one decimal?
+		var value = cultureInfo.numberFormat.decimals ? cultureInfo.numberFormat.decimals > 1 ? 6.51 : 6.5 : 5;
+		var formatted = $.global.format(value, "n", culture);
+		equal( $.global.parseFloat(formatted, 10, culture), value, culture + ": " + formatted );
+	});
+});
+
+test("basics, currency", function() {
+	equal( $.global.parseInt("$5.51"), 5 );
+	equal( $.global.parseFloat("$5.51"), 5.51 );
+	equal( $.global.parseInt("5,51 €", 10, "de-DE"), 5 );
+	equal( $.global.parseFloat("5,51 €", 10, "de-DE"), 5.51 );
+});
+
+test("format-parse all, currency", function() {
+	$.each($.global.cultures, function(culture, cultureInfo) {
+		// TODO is there a better approach to verify cultures with no decimal?
+		var value = cultureInfo.numberFormat.currency.decimals ? 6.51 : 5;
+		var formatted = $.global.format(value, "c", culture);
+		equal( $.global.parseFloat(formatted, 10, culture), value, culture + ": " + formatted );
+	});
+});
--- tests/parseCurrency.js
+++
@@ -1,9 +0,0 @@
-module("parseCurrency");
-
-test("basics", function() {
-	equal( $.global.parseCurrency("$5.51", ""), 5.51 );
-	equal( $.global.parseCurrency("USD 5.51", ""), 5.51 );
-	equal( $.global.parseCurrency("5,51 €", "de-DE"), 5.51 );
-	equal( $.global.parseCurrency("5,51 EUR", "de-DE"), 5.51 );
-	equal( $.global.parseCurrency("€ 5,51", "de-DE"), 5.51 );
-});
