33f2f7aac0ce28199855877c4240a99241cea85f
Richard D. Worth <rdworth@gmail.com>
2011-06-17 11:06:05 -0400

    Refactored in the rest of what was in jquery.global.js except for extend is from jQuery

--- test/localize.js
+++ test/localize.js
@@ -1,40 +1,20 @@
-module('localize', {
-  teardown: function() {
-    Globalize.culture = undefined;
-  }
-});
-
 
 test('set and retrieve translations', function() {
-  Globalize.localize("translate", "fr", "traduire");
-  strictEqual( Globalize.localize("translate", "fr"), "traduire", "translate 'translate' to french" );
-});
-
-test('set and retrieve plugin translations', function() {
-  Globalize.localize("jQuery.localize.plugin", "de", {
-    day: "Tag",
-    month: "Monat"
-  });
-  strictEqual( Globalize.localize("jQuery.localize.plugin", "de").day, "Tag", "translate 'day' to german" );
-});
-
-test('retrieve translations with assigned culture', function() {
-  Globalize.culture = Globalize.cultures.fr;
-  Globalize.localize("translate", "fr", "traduire");
-  strictEqual( Globalize.localize("translate"), "traduire", "translate 'translate' to french" );
-});
-
-test('retrieve plugin translations with assigned culture', function() {
-  Globalize.culture = Globalize.cultures.de;
-  Globalize.localize("jQuery.localize", "de", {
-    day: "Tag",
-    month: "Monat"
-  });
-  strictEqual( Globalize.localize("jQuery.localize").day, "Tag", "translate 'day' to german" );
+	Globalize.addCultureInfo("fr", {
+		messages: {
+			"translate": "traduire"
+		}
+	})
+	strictEqual( Globalize.localize("translate", "fr"), "traduire", "translate 'translate' to french" );
+	strictEqual( Globalize("fr").localize("translate"), "traduire", "translate 'translate' to french" );
 });
 
 test('retrieve transtaltions with new culture', function() {
-  Globalize.culture = "pirate";
-  Globalize.localize("translate", "pirate", "TARRRR");
-  strictEqual( Globalize.localize("translate"), "TARRRR", "translate 'translate' to pirate language" );
+	Globalize.addCultureInfo( "pirate", {
+		messages: {
+			"translate": "TARRRR"
+		}
+	});
+	Globalize.culture("pirate");
+	strictEqual( Globalize.localize("translate"), "TARRRR", "translate 'translate' to pirate language" );
 });
--- test/parse.js
+++ test/parse.js
@@ -7,40 +7,10 @@ test("basics, float", function() {
 	equal( Globalize.parseFloat("5,51", 10, "de-DE"), 5.51 );
 });
 
-test("format-parse all, float", function() {
-	$.each(Globalize.cultures, function(culture, cultureInfo) {
-		// TODO is there a better approach to verify cultures with no/just one decimal?
-		var value = cultureInfo.numberFormat.decimals ? cultureInfo.numberFormat.decimals > 1 ? 6.51 : 6.5 : 5;
-		var formatted = Globalize.format(value, "n", culture);
-		equal( Globalize.parseFloat(formatted, 10, culture), value, culture + ": " + formatted );
-	});
-});
-
 test("basics, currency", function() {
 	equal( Globalize.parseInt("$5.51"), 5 );
 	equal( Globalize.parseFloat("$5.51"), 5.51 );
 	equal( Globalize.parseInt("5,51 €", 10, "de-DE"), 5 );
 	equal( Globalize.parseFloat("5,51 €", 10, "de-DE"), 5.51 );
 	equal( Globalize.parseFloat("5,51 €", "de-DE"), 5.51, "optional radix" );
-});
-
-test("format-parse all, currency", function() {
-	$.each(Globalize.cultures, function(culture, cultureInfo) {
-		// TODO is there a better approach to verify cultures with no decimal?
-		var value = cultureInfo.numberFormat.currency.decimals ? 6.51 : 5;
-		var formatted = Globalize.format(value, "c", culture);
-		equal( Globalize.parseFloat(formatted, culture), value, culture + ": " + formatted );
-	});
-});
-
-test("format-parse all, date", function() {
-	// FIXME
-	return;
-	$.each(Globalize.cultures, function(culture, cultureInfo) {
-		var value = new Date();
-		// TODO is there a better way to get comparable results?
-		value.setTime(0);
-		var formatted = Globalize.format(value, "F", culture);
-		deepEqual( Globalize.parseDate(formatted, "F", culture), value, culture + ": " + formatted );
-	});
 });
