b96ce2537f7851890b29c34563bac7b29efe3946
Richard D. Worth <rdworth@gmail.com>
2011-06-18 19:20:01 -0400

    Removed globinfo dir, superseded by lib/cultures dir.

--- globinfo/Globalization.en-US.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    var cultures = $.global.cultures,
-        en = cultures.en,
-        standard = en.calendars.standard,
-        culture = cultures["en-US"] = $.extend(true, {}, en, {
-
-    }, cultures["en-US"]);
-    culture.calendar = culture.calendars.standard;
-})(Globalization);
