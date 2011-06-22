4e53546b615643837e208d3f48f8d086d49d28c7
Richard D. Worth <rdworth@gmail.com>
2011-06-22 11:28:40 -0400

    Unit tests for issue #44

--- test/parse.js
+++ test/parse.js
@@ -12,6 +12,8 @@ test("basics, float", function() {
 test("basics, currency", function() {
 	equal( Globalize.parseInt("$5.51"), 5 );
 	equal( Globalize.parseFloat("$5.51"), 5.51 );
+	equal( Globalize.parseInt("($5.51)"), -5 );
+	equal( Globalize.parseFloat("($5.51)"), -5.51 );
 	equal( Globalize.parseInt("5,51 €", 10, "de-DE"), 5 );
 	equal( Globalize.parseFloat("5,51 €", 10, "de-DE"), 5.51 );
 	equal( Globalize.parseFloat("5,51 €", "de-DE"), 5.51, "optional radix" );
