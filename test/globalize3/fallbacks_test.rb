77e6831a293c125b7576ee3f5a3f2c3f7a2dc842
Philip Arndt <parndt@gmail.com>
2011-09-13 21:27:23 +1200

    Add more tests to the 'creating just one translation when fallbacks set' case and explicitly removed all other fallbacks.

--- test/globalize3/fallbacks_test.rb
+++ test/globalize3/fallbacks_test.rb
@@ -5,7 +5,7 @@ class TranslatedTest < Test::Unit::TestCase
     @previous_backend = I18n.backend
     I18n.pretend_fallbacks
     I18n.backend = BackendWithFallbacks.new
-
+     
     I18n.locale = :'en-US'
     I18n.fallbacks = ::I18n::Locale::Fallbacks.new
   end
@@ -19,13 +19,13 @@ class TranslatedTest < Test::Unit::TestCase
   test "keeping one field in new locale when other field is changed" do
     I18n.fallbacks.map('de-DE' => [ 'en-US' ])
     post = Post.create :title => 'foo'
-
+  
     I18n.locale = 'de-DE'
     post.content = 'bar'
-
+  
     assert_equal 'foo', post.title
   end
-
+  
   test "modifying non-required field in a new locale" do
     I18n.fallbacks.map 'de-DE' => [ 'en-US' ]
     post = Post.create :title => 'foo'
@@ -33,34 +33,34 @@ class TranslatedTest < Test::Unit::TestCase
     post.content = 'bar'
     assert post.save
   end
-
+  
   test "resolves a simple fallback" do
     I18n.locale = 'de-DE'
     post = Post.create :title => 'foo'
-
+  
     I18n.locale = 'de'
     post.title = 'baz'
     post.content = 'bar'
     post.save!
-
+  
     I18n.locale = 'de-DE'
     assert_equal 'foo', post.title
     assert_equal 'bar', post.content
   end
-
+  
   test "resolves a simple fallback without reloading" do
     I18n.locale = 'de-DE'
     post = Post.new :title => 'foo'
-
+  
     I18n.locale = 'de'
     post.title = 'baz'
     post.content = 'bar'
-
+  
     I18n.locale = 'de-DE'
     assert_equal 'foo', post.title
     assert_equal 'bar', post.content
   end
-
+  
   test "resolves a complex fallback without reloading" do
     I18n.fallbacks.map 'de' => %w(en he)
     I18n.locale = 'de'
@@ -74,76 +74,84 @@ class TranslatedTest < Test::Unit::TestCase
     assert_equal 'foo', post.title
     assert_equal 'bar', post.content
   end
-
+  
   test 'fallbacks with lots of locale switching' do
     I18n.fallbacks.map :'de-DE' => [ :'en-US' ]
     post = Post.create :title => 'foo'
-
+  
     I18n.locale = :'de-DE'
     assert_equal 'foo', post.title
-
+  
     I18n.locale = :'en-US'
     post.update_attributes(:title => 'bar')
-
+  
     I18n.locale = :'de-DE'
     assert_equal 'bar', post.title
   end
-
+  
   test 'fallbacks with lots of locale switching 2' do
     I18n.fallbacks.map :'de-DE' => [ :'en-US' ]
     child = Child.create :content => 'foo'
-
+  
     I18n.locale = :'de-DE'
     assert_equal 'foo', child.content
-
+  
     I18n.locale = :'en-US'
     child.update_attributes(:content => 'bar')
-
+  
     I18n.locale = :'de-DE'
     assert_equal 'bar', child.content
   end
-
+  
   test 'fallbacks with nil translations' do
     I18n.fallbacks.map :'de-DE' => [ :'en-US' ]
     post = Post.create :title => 'foo'
-
+  
     I18n.locale = :'de-DE'
     assert_equal 'foo', post.title
-
+  
     post.update_attribute :title, nil
     assert_equal 'foo', post.title
   end
-
+  
   test 'fallbacks with empty translations' do
     I18n.fallbacks.map :'de-DE' => [ :'en-US' ]
     task = Task.create :name => 'foo'
-
+  
     I18n.locale = :'de-DE'
     assert_equal 'foo', task.name
-
+  
     task.update_attribute :name, ''
     assert_equal 'foo', task.name
   end
-
+  
   test 'fallbacks with empty translations 2' do
     I18n.fallbacks.map :'de-DE' => [ :'en-US' ]
     task = Task.create :name => 'foo'
     post = Post.create :title => 'foo'
-
+  
     I18n.locale = :'de-DE'
     assert_equal 'foo', task.name
     assert_equal 'foo', post.title
-
+  
     task.update_attribute :name, ''
     assert_equal 'foo', task.name
-
+  
     post.update_attribute :title, ''
     assert_equal '', post.title
   end
 
   test 'creating just one translation when fallbacks set' do
-    I18n.fallbacks.map :'de-DE' => [ :'en-US' ]
-    task = Task.create :name => 'foo'
+    I18n.fallbacks.clear
+    I18n.fallbacks.map :de => [ :en ]
+    I18n.locale = :de
+    
+    task = Task.new :name => 'foo'
+
+    assert_equal true, task.translations.empty?(&:persisted?)
+    assert_equal 2, task.translations.length
+
+    task.save
 
     assert_equal 1, task.translations.length
   end
