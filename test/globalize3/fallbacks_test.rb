29f0c379e11c6e4d95a9c917ab858f88a3199cbe
Michael Reinsch <michael@mobalean.com>
2012-03-09 19:51:57 +0900

    get fallbacks from record, so we can hook into this to allow for model specific language fallbacks

--- test/globalize3/fallbacks_test.rb
+++ test/globalize3/fallbacks_test.rb
@@ -152,6 +152,25 @@ class TranslatedTest < Test::Unit::TestCase
 
     assert_equal [:de], task.translations.map(&:locale).sort
   end
+
+  test 'fallback overwritten in model' do
+    I18n.fallbacks.clear
+    Task.fallbacks = [:en, :de]
+    I18n.locale = :de
+
+    task = Task.create(:name => 'foo')
+    assert_equal 'foo', task.name
+
+    I18n.locale = :en
+    assert_equal 'foo', task.name
+    task.name = 'bar'
+    assert_equal 'bar', task.name
+
+    I18n.locale = :fr
+    assert_equal 'bar', task.name
+
+    Task.fallbacks = nil
+  end
 end
 # TODO should validate_presence_of take fallbacks into account? maybe we need
 #   an extra validation call, or more options for validate_presence_of.
