387f46026dae06c8a02510ee8e5d2a7275486887
Chris Salzberg <chrissalzberg@gmail.com>
2012-03-19 16:33:15 +0900

    undo refactoring, should match base class id not translation class id.

--- lib/patches/active_record/uniqueness_validator.rb
+++ lib/patches/active_record/uniqueness_validator.rb
@@ -8,7 +8,7 @@ ActiveRecord::Validations::UniquenessValidator.class_eval do
       table = finder_class.arel_table
 
       relation = build_relation(finder_class, table, attribute, value).and(table[:locale].eq(Globalize.locale))
-      relation = relation.and(table[:id].not_eq(record.send(:id))) if record.persisted?
+      relation = relation.and(table[:"#{klass.name.downcase}_id"].not_eq(record.send(:id))) if record.persisted?
 
 #      TODO: add scope with translated attributes
 
