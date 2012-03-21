7455ac3d7d9c98700fd4e0764dc3e473c92de73b
Ranco Marcus <ranco.marcus@epirion.nl>
2012-03-21 07:56:46 +0100

    fixed parsing of group separators.

--- test/parse.js
+++ test/parse.js
@@ -36,6 +36,45 @@ test("basics, date", function() {
 	equal( Globalize.parseDate('2011-17-11 13:23:12','yyyy-dd-MM HH:mm:ss','fr-CA').valueOf(), (new Date(2011, 10, 17, 13, 23, 12)).valueOf() );
 });
 
+test("basics, group separators", function() {
+	// Native
+	equal( parseFloat("500,600"), 500)
+	equal( parseFloat("500a600"), 500)
+	
+	// Globalize - using "en" culture that has a "," group separator and a "." decimal separator 
+	Globalize.culture("en");
+	ok( isNaN( Globalize.parseFloat("500 600")));
+	ok( isNaN( Globalize.parseFloat("500a600")));
+	
+	// Globalize - using "fr" culture that has a " " group separator and a "," decimal separator
+	Globalize.culture("fr");
+	equal( Globalize.parseFloat("500.600"), 500.6);
+	ok( isNaN( Globalize.parseFloat("500a600")));
+		
+	// Globalize - using "nl" culture that has a "." group separator and a "," decimal separator
+	Globalize.culture("nl");
+	equals( Globalize.parseFloat("1"), 1);
+	equals( Globalize.parseFloat("1,01"), 1.01);
+	equals( Globalize.parseFloat("10"), 10);
+	equals( Globalize.parseFloat("10,01"), 10.01);
+	equals( Globalize.parseFloat("100"), 100);
+	equals( Globalize.parseFloat("100,01"), 100.01);
+	equals( Globalize.parseFloat("1.000"), 1000);
+	equals( Globalize.parseFloat("1.000,01"), 1000.01);
+	equals( Globalize.parseFloat("10.000"), 10000);
+	equals( Globalize.parseFloat("10.000,01"), 10000.01);
+	
+	// incorrect group separators
+	ok( isNaN( Globalize.parseFloat("1.")));
+	ok( isNaN( Globalize.parseFloat("1.0")));
+	ok( isNaN( Globalize.parseFloat("1.00")));
+	ok( isNaN( Globalize.parseFloat("10.00")));
+	ok( isNaN( Globalize.parseFloat("100.00")));
+	ok( isNaN( Globalize.parseFloat("1000.00")));	
+	ok( isNaN( Globalize.parseFloat("10000.00")));
+	ok( isNaN( Globalize.parseFloat("10000.000")));
+});
+
 test("invalid input", function() {
 	// #47 - Return NaN instead of 0 for invalid values
 	ok( isNaN(Globalize.parseInt("foo")) );
