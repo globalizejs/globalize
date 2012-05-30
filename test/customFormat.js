5bcc7d675af175ac9bb928715e4ce31551d146a4
erethnor <razor@therazor.net>
2012-05-30 11:58:14 +0200

    Completely rewritten the code that handles mixed text/format - now working almost properly. Fixed some unit tests

--- test/customFormat.js
+++ test/customFormat.js
@@ -64,7 +64,7 @@ test("Number Formatting - custom w/ pattern for +/-", function() {
 	equal( Globalize.format(-1234.567, "Positive: 0;Negative: 0;zero;ignored"), "Negative: 1235" );
 	equal( Globalize.format(0, "Positive: 0;Negative: 0;zero;ignored"), "zero" );
 	equal( Globalize.format(0.00001, "Positive: 0;Negative: 0;zero;ignored"), "zero" );
-	equal( Globalize.format(0.00001, "Positive: 0;Negative: 0.0000#;zero;ignored"), "Negative: 0.00001" );
+	equal( Globalize.format(-0.00001, "Positive: 0;Negative: 0.0000#;zero;ignored"), "Negative: 0.00001" );
 	equal( Globalize.format(0.00001, "Positive: 0;Negative: 0;;ignored"), "Positive: 0" );
 	equal( Globalize.format(-0.00001, "Positive: 0;Negative: 0;;ignored"), "Positive: 0" );
 	// the test below gives a different result (zero) in C#.
@@ -76,11 +76,15 @@ test("Number Formatting - custom w/ pattern for +/-", function() {
 });
 
 test("Number Formatting - custom w/ non-formatting text", function() {
-	equal( Globalize.format(1234.567, "# 0 # # 0 0|0.0|0 0 0 # #"), " 0 0 1 2 3|4.5|6 7 0" );
-	equal( Globalize.format(-1234.567, "# 0 # # 0 0|0.0|0 0 0 # #"), "- 0 0 1 2 3|4.5|6 7 0" );
-	equal( Globalize.format(-1234.567, "0 # # 0 0|0.0|0 0 0 # #"), "-0 0 1 2 3|4.5|6 7 0" );
-	equal( Globalize.format(-1234.567, "0 # # 0 \\0|0.0|0 0 0 # #"), "-0 1 2 3 0|4.5|6 7 0" );
-	equal( Globalize.format(1234.567, "<a href=\"\\#go\">#</a>"), "<a href=\"#go\">1235</a>" );
+	equal( Globalize.format(5417543010, "(000) 000-0000"), "(541) 754-3010" );
+	equal( Globalize.format(123123, "0000|,|000"), "0,123,||123" );
+	equal( Globalize.format(1234.567, "# 0 # # 0 0|0.0|0 0 0 # #"), " 0 0 1 2 3|4.5|6 7 0  " );
+	equal( Globalize.format(1234.567, "# 0 # #,000.0|0 0 0 # #"), " 0 0 1,234.5|6 7 0  " );
+	equal( Globalize.format(1234.567, "# 0 # #,000|.|0|0 0 0 # #"), " 0 0 1,234|.|5|6 7 0  " );
+	equal( Globalize.format(-1234.567, "# 0 # # 0 0|0.0|0 0 0 # #"), "- 0 0 1 2 3|4.5|6 7 0  " );
+	equal( Globalize.format(-1234.567, "0 # # 0 0|0.0|0 0 0 # #"), "-0 0 1 2 3|4.5|6 7 0  " );
+	equal( Globalize.format(-1234.567, "0 # # 0 \\0|0.0|0 0 0 # #"), "-0 1 2 3 0|4.5|6 7 0  " );
+	equal( Globalize.format(-1234.567, "<a href=\"\\#go\">#</a>"), "<a href=\"#go\">-1235</a>" );
 	equal( Globalize.format(1234.567, '\\\\00000'), "\\01235" );
 });
 
@@ -97,7 +101,7 @@ test("Number Formatting - custom w/ various edge cases", function() {
 	equal( Globalize.format(1234.567, "000, ,000, ,000.00"), "000, 001, 234.57" );
 	equal( Globalize.format(1234.567, "00 00, ,00, ,00.00"), "00, 00 1,2 34.57" );
 	equal( Globalize.format(1234567890, "##0bill ##0mill ##0thousands and ##0"), "1bill 234mill 567thousands and 890" );
-	equal( Globalize.format(12,23, "##0bill ##0mill ##0thousands and ##0"), "0bill 000mill 000thousands and 012" );
+	equal( Globalize.format(12.23, "##0bill ##0mill ##0thousands and ##0"), "0bill 000mill 000thousands and 012" );
 	equal( Globalize.format(0.1234, ".####"), ".1234" );
 	equal( Globalize.format(0.1234, ".## ##"), ".12 34" );
 	equal( Globalize.format(0.1234, "."), "" );
