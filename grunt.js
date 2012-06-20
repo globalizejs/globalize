491ce598bb3995a38e7add859ec42196a14ca7af
erethnor <razor@therazor.net>
2012-06-20 14:52:48 +0200

    rewritten grunt file, seemed to be broken

--- grunt.js
+++ grunt.js
@@ -1,21 +1,25 @@
-config.init({
-	pkg: "<json:package.json>",
-	lint: {
-		files: [ "lib/globalize.js", "lib/cultures/*.js", "test/*.js" ]
-	},
-	qunit: {
-		files: [ "test/*.html" ]
-	},
-	watch: {
-		files: [ "<config:lint.files>", "test/*.html" ],
-		tasks: "lint qunit"
-	},
-	jshint: {
-		options: {
-			eqnull: true
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: "<json:package.json>",
+		lint: {
+			all: [ "lib/globalize.js", "lib/cultures/*.js", "test/*.js" ]
+		},
+		qunit: {
+			all: [ "test/*.html" ]
+		},
+		watch: {
+			scripts: {
+				files: [ "<config:lint.files>", "test/*.html" ]
+			},
+			tasks: "lint qunit"
+		},
+		jshint: {
+			options: {
+				eqnull: true
+			}
 		}
-	}
-});
+	});
 
-// Default task.
-task.registerTask( "default", "lint qunit" );
+	// Default task.
+	grunt.registerTask( "default", "lint qunit" );
+};
