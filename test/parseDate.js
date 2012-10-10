5a8de49d50ef9047f54b7b5e9386d6648902f52a
Dan Turner <dan.turner@singularityit.com>
2012-10-10 12:10:14 +1100

    Created a parseDate test module and moved the tests related to date parsing from parseFloat to this module

---
+++ test/parseDate.js
@@ -0,0 +1,8 @@
+module("parseDate", lifecycle );
+
+test("basics, date", function() {
+	equal( Globalize.parseDate('2011/17/11 13:23:12','yyyy/dd/MM HH:mm:ss','fr').valueOf(), (new Date(2011, 10, 17, 13, 23, 12)).valueOf() );
+	equal( Globalize.parseDate('2011-17-11 13:23:12','yyyy-dd-MM HH:mm:ss','fr').valueOf(), (new Date(2011, 10, 17, 13, 23, 12)).valueOf() );
+	equal( Globalize.parseDate('2011/17/11 13:23:12','yyyy/dd/MM HH:mm:ss','fr-CA').valueOf(), (new Date(2011, 10, 17, 13, 23, 12)).valueOf() );
+	equal( Globalize.parseDate('2011-17-11 13:23:12','yyyy-dd-MM HH:mm:ss','fr-CA').valueOf(), (new Date(2011, 10, 17, 13, 23, 12)).valueOf() );
+});
