5c2b86ff1b1257e46aac4a80af2733623d77bafe
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-10-18 12:15:37 -0400

    Update grunt file to current grunt standards; add min target, fixed #35

--- grunt.js
+++ grunt.js
@@ -1,5 +1,14 @@
-config.init({
+module.exports = function( grunt ) {
+
+grunt.initConfig({
 	pkg: "<json:package.json>",
+	meta: {
+		banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
+				"<%= grunt.template.today('isoDate') %>\n" +
+				"<%= pkg.homepage ? '* ' + pkg.homepage + '\n' : '' %>" +
+				"* Copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>;" +
+				" Licensed <%= _.pluck(pkg.licenses, 'type').join(', ') %> */"
+	},
 	lint: {
 		files: [ "lib/globalize.js", "lib/cultures/*.js", "test/*.js" ]
 	},
@@ -14,8 +23,14 @@ config.init({
 		options: {
 			eqnull: true
 		}
+	},
+	min: {
+		"dist/globalize.min.js": [ "<banner:meta.banner>", "lib/globalize.js" ]
 	}
 });
 
 // Default task.
-task.registerTask( "default", "lint qunit" );
+grunt.registerTask( "default", "lint qunit" );
+
+}
+
