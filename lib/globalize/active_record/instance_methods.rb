e45ecbe4c8292851fb43ecc7b95504ad36ac41ce
Andrey Chernih <andrey.chernih@gmail.com>
2012-12-14 10:23:27 +0400

    Do not hit database each time missing translation requested

--- lib/globalize/active_record/instance_methods.rb
+++ lib/globalize/active_record/instance_methods.rb
@@ -135,7 +135,7 @@ module Globalize
         unless @translation_caches[locale]
           # Fetch translations from database as those in the translation collection may be incomplete
           _translation = translations.detect{|t| t.locale.to_s == locale.to_s}
-          _translation ||= translations.with_locale(locale).first
+          _translation ||= translations.with_locale(locale).first unless translations.loaded?
           _translation ||= translations.build(:locale => locale)
           @translation_caches[locale] = _translation
         end
