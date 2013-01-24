a6d1a8fda3ae72ddbec70b9c4b593894a338a359
Philip Arndt <parndt@gmail.com>
2013-01-25 10:44:46 +1300

    Allow translates to be called again with new attribute names.
    Fixes #189

--- lib/globalize/active_record/act_macro.rb
+++ lib/globalize/active_record/act_macro.rb
@@ -4,45 +4,19 @@ module Globalize
       def translates(*attr_names)
 
         options = attr_names.extract_options!
+        setup_translates!(options) unless translates?
+
         attr_names = attr_names.map(&:to_sym)
+        attr_names -= translated_attribute_names if defined?(translated_attribute_names)
 
-        unless translates?
-          options[:table_name] ||= "#{table_name.singularize}_translations"
-          options[:foreign_key] ||= class_name.foreign_key
-
-          class_attribute :translated_attribute_names, :translation_options, :fallbacks_for_empty_translations
-          self.translated_attribute_names = []
-          self.translation_options        = options
-          self.fallbacks_for_empty_translations = options[:fallbacks_for_empty_translations]
-
-          include InstanceMethods
-          extend  ClassMethods, Migration
-
-          translation_class.table_name = options[:table_name] if translation_class.table_name.blank?
-
-          has_many :translations, :class_name  => translation_class.name,
-                                  :foreign_key => options[:foreign_key],
-                                  :dependent   => :destroy,
-                                  :extend      => HasManyExtensions
-
-          after_create :save_translations!
-          after_update :save_translations!
-
-          if options[:versioning]
-            ::ActiveRecord::Base.extend(Globalize::Versioning::PaperTrail)
-
-            translation_class.has_paper_trail
-            delegate :version, :versions, :to => :translation
-          end
-
+        if attr_names.present?
           translation_class.instance_eval %{
-            attr_accessible :#{(attr_names | [:locale]).join(', :')}
+            attr_accessible :#{attr_names.join(', :')}
           }
 
-          # detect and apply serialization
           attr_names.each do |attr_name|
+            # Detect and apply serialization.
             serializer = self.serialized_attributes[attr_name.to_s]
-
             if serializer.present?
               if defined?(::ActiveRecord::Coders::YAMLColumn) &&
                  serializer.is_a?(::ActiveRecord::Coders::YAMLColumn)
@@ -52,15 +26,15 @@ module Globalize
 
               translation_class.send :serialize, attr_name, serializer
             end
+
+            # Create accessors for the attribute.
+            translated_attr_accessor(attr_name)
+            translations_accessor(attr_name)
+
+            # Add attribute to the list.
+            self.translated_attribute_names << attr_name
           end
         end
-
-        new_attr_names = attr_names - translated_attribute_names
-        new_attr_names.each do |attr_name|
-          translated_attr_accessor(attr_name)
-          translations_accessor(attr_name)
-        end
-        self.translated_attribute_names += new_attr_names
       end
 
       def class_name
@@ -72,6 +46,39 @@ module Globalize
 
       def translates?
         included_modules.include?(InstanceMethods)
+      end
+
+      protected
+      def setup_translates!(options)
+        options[:table_name] ||= "#{table_name.singularize}_translations"
+        options[:foreign_key] ||= class_name.foreign_key
+
+        class_attribute :translated_attribute_names, :translation_options, :fallbacks_for_empty_translations
+        self.translated_attribute_names = []
+        self.translation_options        = options
+        self.fallbacks_for_empty_translations = options[:fallbacks_for_empty_translations]
+
+        include InstanceMethods
+        extend  ClassMethods, Migration
+
+        translation_class.table_name = options[:table_name] if translation_class.table_name.blank?
+
+        has_many :translations, :class_name  => translation_class.name,
+                                :foreign_key => options[:foreign_key],
+                                :dependent   => :destroy,
+                                :extend      => HasManyExtensions
+
+        after_create :save_translations!
+        after_update :save_translations!
+
+        if options[:versioning]
+          ::ActiveRecord::Base.extend(Globalize::Versioning::PaperTrail)
+
+          translation_class.has_paper_trail
+          delegate :version, :versions, :to => :translation
+        end
+
+        translation_class.instance_eval %{ attr_accessible :locale }
       end
     end
 
