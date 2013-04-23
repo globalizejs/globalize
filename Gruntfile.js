bce7fe45815178431a59d532d36bf94922cf7319
CTAPbIu_MABP <ctapbiumabp@gmail.com>
2013-04-23 13:12:05 +0800

    grunt 4.0.1

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+
+	"use strict";
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		jshint: {
+			dist: {
+				src: [ "lib/globalize.js", "lib/cultures/*.js" ],
+				options: {
+					jshintrc: ".jshintrc"
+				}
+			},
+			grunt: {
+				src: [ "Gruntfile.js" ],
+				options: {
+					jshintrc: ".jshintrc"
+				}
+			},
+			test: {
+				src: [ "test/*.js" ],
+				options: {
+					jshintrc: "test/.jshintrc"
+				}
+			}
+		},
+		uglify: {
+			dist: {
+				options: {
+					beautify: {
+						ascii_only: true
+					},
+					banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
+						"<%= grunt.template.today('isoDate') %>\n" +
+						"<%= pkg.homepage ? '* ' + pkg.homepage + '\\n' : '' %>" +
+						"* Copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>;" +
+						" Licensed <%= _.pluck(pkg.licenses, 'type').join(', ') %> */\n"
+				},
+				files: {
+					"dist/globalize.min.js": [ "lib/globalize.js" ]
+				}
+			}
+		},
+		qunit: {
+			files: [ "test/*.html" ]
+		},
+		watch: {
+			files: [ "lib/globalize.js", "lib/cultures/*.js", "test/*.js", "test/*.html" ],
+			tasks: "jshint qunit"
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-qunit");
+
+	// Default task.
+	grunt.registerTask( "default", ["jshint", "uglify", "qunit"] );
+
+};
+
