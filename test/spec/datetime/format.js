11de042dd173cc75d3660783f5cd633b3289d1ea
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-10-11 17:57:12 -0300

    Datetime: Add format milliseconds (A+) tests

--- test/spec/datetime/format.js
+++ test/spec/datetime/format.js
@@ -295,6 +295,14 @@ define([
 			expect( Globalize.format( date2, "SSSSS" ) ).to.equal( "36900" );
 		});
 
+		it( "should format various milliseconds (A+)", function() {
+			expect( Globalize.format( date2, "A" ) ).to.equal( "633074" );
+			expect( Globalize.format( date2, "AA" ) ).to.equal( "6330737" );
+			expect( Globalize.format( date2, "AAA" ) ).to.equal( "63307369" );
+			expect( Globalize.format( date2, "AAAA" ) ).to.equal( "633073690" );
+			expect( Globalize.format( date2, "AAAAA" ) ).to.equal( "6330736900" );
+		});
+
 		/**
 		 *  Zone
 		 */
