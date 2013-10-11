a9823011c35ae7c7a30ae5520c4c105300f28bb7
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-10-11 11:58:43 -0300

    Datetime: Create start-of
    - Simplify datetime/milliseconds-in-day using datetime/start-of;

--- src/datetime/milliseconds-in-day.js
+++ src/datetime/milliseconds-in-day.js
@@ -1,18 +1,13 @@
-define(function() {
+define([
+	"./start-of"
+], function( datetimeStartOf ) {
 
 	/**
 	 * millisecondsInDay
 	 */
 	return function( date ) {
 		// TODO Handle daylight savings discontinuities
-		// TODO there must be an easier way.
-		var truncated = new Date( date );
-		truncated.setMilliseconds( 0 );
-		truncated.setSeconds( 0 );
-		truncated.setMinutes( 0 );
-		truncated.setHours( 0 );
-		// TODO Does the below subtraction work on all supported browsers?
-		return date - truncated;
+		return date - datetimeStartOf( date, "day" );
 	};
 
 });
---
+++ src/datetime/start-of.js
@@ -0,0 +1,32 @@
+define(function() {
+
+	/**
+	 * startOf
+	 *
+	 * Return the 
+	 */
+	return function( date, unit ) {
+		date = new Date( date.getTime() );
+		switch( unit ) {
+			case "year":
+				date.setMonth( 0 );
+			/* falls through */
+			case "month":
+				date.setDate( 1 );
+			/* falls through */
+			case "day":
+				date.setHours( 0 );
+			/* falls through */
+			case "hour":
+				date.setMinutes( 0 );
+			/* falls through */
+			case "minute":
+				date.setSeconds( 0 );
+			/* falls through */
+			case "second":
+				date.setMilliseconds( 0 );
+		}
+		return date;
+	};
+
+});
