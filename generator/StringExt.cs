e7f8325f80b2cb15a1e700cbdd2977012b59b791
Tobias Weibel <tobias.weibel@autodesk.com>
2013-10-16 12:24:00 +0200

    Cleaned up code and updated to latest .net 4.5

--- generator/StringExt.cs
+++ generator/StringExt.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Globalization;
 
 namespace Globalization {
     public static class StringExtensions {
