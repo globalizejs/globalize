53a0c62c237f5df351d94cd65f765a97297ae199
Chris Salzberg <chrissalzberg@gmail.com>
2013-12-11 13:48:53 +0900

    remove versioning table.

--- test/data/schema.rb
+++ test/data/schema.rb
@@ -127,18 +127,6 @@ ActiveRecord::Schema.define do
     t.string     :locale,    :default => 'en'
   end
 
-  create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
-    t.string   "whodunnit"
-    t.text     "object"
-    t.string   "locale"
-    t.datetime "created_at"
-  end
-
-  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
-
   create_table 'UPPERCASE_TABLE_NAME', :force => true do |t|
     t.string :name
   end
