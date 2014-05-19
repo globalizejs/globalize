37df5ee91b4c0382295bafbdaaa379d4d63587ac
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-05-19 15:58:04 -0700

    Test: Reduce boilerplate code by using a helper
    Closes #247

--- test/functional/date/parse.js
+++ test/functional/date/parse.js
@@ -20,28 +20,32 @@ Globalize.locale( "en" );
 
 module( "Datetime Parse" );
 
+function assertParseDate( input, options, output ) {
+	deepEqual( Globalize.parseDate( input, options ), output, JSON.stringify( options ) );
+}
+
 test( "should parse skeleton", function() {
 	date = new Date();
 	date.setDate( 15 );
 	date = startOf( date, "day" );
-	deepEqual( Globalize.parseDate( "15", { skeleton: "d" } ), date, "{ skeleton: \"d\" }" );
-	deepEqual( Globalize.parseDate( "15 Wed", { skeleton: "Ed" } ), date, "{ skeleton: \"Ed\" }" );
+	assertParseDate( "15", { skeleton: "d" }, date );
+	assertParseDate( "15 Wed", { skeleton: "Ed" }, date );
 
 	date = new Date();
 	date.setHours( 17 );
 	date.setMinutes( 35 );
 	date.setSeconds( 7 );
 	date = startOf( date, "second" );
-	deepEqual( Globalize.parseDate( "Wed 5:35:07 PM", { skeleton: "Ehms" } ), date, "{ skeleton: \"Ehms\" }" );
+	assertParseDate( "Wed 5:35:07 PM", { skeleton: "Ehms" }, date );
 
 	date = new Date( 2010, 8, 15 );
 	date = startOf( date, "day" );
-	deepEqual( Globalize.parseDate( "Wed, Sep 15, 2010 AD", { skeleton: "GyMMMEd" } ), date, "{ skeleton: \"GyMMMEd\" }" );
-	deepEqual( Globalize.parseDate( "9/15/2010", { skeleton: "yMd" } ), date, "{ skeleton: \"yMd\" }" );
+	assertParseDate( "Wed, Sep 15, 2010 AD", { skeleton: "GyMMMEd" }, date );
+	assertParseDate( "9/15/2010", { skeleton: "yMd" }, date );
 
 	date = new Date( 2010, 0 );
 	date = startOf( date, "year" );
-	deepEqual( Globalize.parseDate( "Q3 2010", { skeleton: "yQQQ" } ), date, "{ skeleton: \"yQQQ\" }" );
+	assertParseDate( "Q3 2010", { skeleton: "yQQQ" }, date );
 
 	// Via instance globalize.parseDate().
 	deepEqual( Globalize( "pt" ).parseDate( "2010 T3", { skeleton: "yQQQ" } ), date, "{ skeleton: \"yQQQ\" }" );
@@ -53,34 +57,34 @@ test( "should parse time presets", function() {
 	date.setMinutes( 35 );
 	date.setSeconds( 7 );
 	date = startOf( date, "second" );
-	deepEqual( Globalize.parseDate( "5:35:07 PM", { time: "medium" } ), date, "{ time: \"medium\" }" );
+	assertParseDate( "5:35:07 PM", { time: "medium" }, date );
 	date = startOf( date, "minute" );
-	deepEqual( Globalize.parseDate( "5:35 PM", { time: "short" } ), date, "{ time: \"short\" }" );
+	assertParseDate( "5:35 PM", { time: "short" }, date );
 });
 
 test( "should parse date presets", function() {
 	date = new Date( 2010, 8, 15 );
 	date = startOf( date, "day" );
-	deepEqual( Globalize.parseDate( "Wednesday, September 15, 2010", { date: "full" } ), date, "{ date: \"full\" }" );
-	deepEqual( Globalize.parseDate( "September 15, 2010", { date: "long" } ), date, "{ date: \"long\" }" );
-	deepEqual( Globalize.parseDate( "Sep 15, 2010", { date: "medium" } ), date, "{ date: \"medium\" }" );
-	deepEqual( Globalize.parseDate( "9/15/10", { date: "short" } ), date, "{ date: \"short\" }" );
+	assertParseDate( "Wednesday, September 15, 2010", { date: "full" }, date );
+	assertParseDate( "September 15, 2010", { date: "long" }, date );
+	assertParseDate( "Sep 15, 2010", { date: "medium" }, date );
+	assertParseDate( "9/15/10", { date: "short" }, date );
 });
 
 test( "should parse datetime presets", function() {
 	date = new Date( 2010, 8, 15 );
 	date = startOf( date, "day" );
-	deepEqual( Globalize.parseDate( "Wednesday, September 15, 2010", { date: "full" } ), date, "{ date: \"full\" }" );
+	assertParseDate( "Wednesday, September 15, 2010", { date: "full" }, date );
 
 	date = new Date( 2010, 8, 15, 17, 35, 7 );
 	date = startOf( date, "second" );
-	deepEqual( Globalize.parseDate( "Sep 15, 2010, 5:35:07 PM", { datetime: "medium" } ), date, "{ datetime: \"medium\" }" );
+	assertParseDate( "Sep 15, 2010, 5:35:07 PM", { datetime: "medium" }, date );
 });
 
 test( "should parse raw patterns", function() {
 	date = new Date( 2010, 8, 15 );
 	date = startOf( date, "day" );
-	deepEqual( Globalize.parseDate( "Wed, Sep 15, 2010 AD", { pattern: "E, MMM d, y G" } ), date, "{ pattern: \"E, MMM d, y G\" }" );
+	assertParseDate( "Wed, Sep 15, 2010 AD", { pattern: "E, MMM d, y G" }, date );
 });
 
 test( "should parse a formatted date (reverse operation test)", function() {
