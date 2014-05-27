3e65fd05d35113f7fbc5cc713591c4a8b7c072f8
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-05-27 11:22:11 -0700

    Build

---
+++ dist/globalize-allinone-node.js
@@ -0,0 +1,19 @@
+/*!
+ * Globalize v1.0.0-alpha.3
+ *
+ * http://github.com/jquery/globalize
+ *
+ * Copyright 2005, 2014 jQuery Foundation, Inc. and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2014-05-27T18:22Z
+ */
+
+// Core
+module.exports = require( "./globalize" );
+
+// Extent core with the following modules
+require( "./globalize/date" );
+require( "./globalize/number" );
+require( "./globalize/message" );
