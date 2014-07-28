419e52a4bf9493643ddeac00603a8f9e8226cb30
Bao Ngo <bingo@planwise.com>
2014-07-27 18:57:37 -0700

    Validation/normalization for number of digits
    We add in checks for formatNumber options to make sure the options are
    valid.

--- src/number/format.js
+++ src/number/format.js
@@ -41,15 +41,89 @@ return function( number, pattern, cldr, options ) {
 	round = numberRound( options.round );
 	properties = numberPatternProperties( pattern[ 0 ] );
 	padding = properties[ 1 ];
-	minimumIntegerDigits = options.minimumIntegerDigits || properties[ 2 ];
-	minimumFractionDigits = options.minimumFractionDigits || properties[ 3 ];
-	// In case options.maximumFractionDigits is passed in with value of 0, which truncates the fractional portion
-	maximumFractionDigits = typeof options.maximumFractionDigits === "number" ? options.maximumFractionDigits : properties[ 4 ];
-	minimumSignificantDigits = options.minimumSignificantDigits || properties[ 5 ];
-	maximumSignificantDigits = options.maximumSignificantDigits || properties[ 6 ];
+	minimumIntegerDigits = options.hasOwnProperty("minimumFractionDigits") ? options.minimumIntegerDigits : properties[ 2 ];
+	minimumFractionDigits = options.hasOwnProperty("minimumFractionDigits") ? options.minimumFractionDigits : properties[ 3 ];
+	maximumFractionDigits = options.hasOwnProperty("maximumFractionDigits") ? options.maximumFractionDigits : properties[ 4 ];
+	minimumSignificantDigits = options.hasOwnProperty("minimumSignificantDigits") ? options.minimumSignificantDigits : properties[ 5 ];
+	maximumSignificantDigits = options.hasOwnProperty("maximumSignificantDigits") ? options.maximumSignificantDigits : properties[ 6 ];
 	roundIncrement = properties[ 7 ];
 	primaryGroupingSize = properties[ 8 ];
 	secondaryGroupingSize = properties[ 9 ];
+
+	// Validate and/or normalize number of digits
+	if ( isFinite(minimumIntegerDigits) ) {
+		if ( minimumIntegerDigits < 1 || minimumIntegerDigits > 21 ) {
+			throw new Error( "Minimum integer digits is out of range" );
+		}
+	} else {
+		throw new Error( "Minimum integer digits not a valid integer" );
+	}
+
+	if ( isFinite(minimumFractionDigits) ) {
+		if ( minimumFractionDigits < 0 || minimumFractionDigits > 20 ) {
+			throw new Error( "Minimum fraction digits is out of range" );
+		}
+	} else {
+		throw new Error( "Minimum fraction digits not a valid integer" );
+	}
+
+	if ( isFinite(maximumFractionDigits) ) {
+		if ( maximumFractionDigits < 0 || minimumFractionDigits > 20 ) {
+			throw new Error( "Maximum fraction digits is out of range" );
+		}
+	} else {
+		throw new Error( "Maximum fraction digits not a valid integer" );
+	}
+
+	if ( minimumFractionDigits > maximumFractionDigits ) {
+
+		// Cases where minimum and maximum fraction digits options are provided
+		if ( options.hasOwnProperty("minimumFractionDigits") && options.hasOwnProperty("maximumFractionDigits") ) {
+			throw new Error( "Minimum fraction digits cannot be greater than its maximum" );
+
+		// Minimum fraction digits option provided but maximum omitted
+		} else if ( options.hasOwnProperty("minimumFractionDigits") ) {
+
+			// e.g. For 3.14159 where minimumFractionDigits=5, a pattern-provided maximumFractionDigits=3 would have to be normalized to 5
+			maximumFractionDigits = minimumFractionDigits;
+
+		// Maximum fraction digits option provided but minimum omitted
+		} else if ( options.hasOwnProperty("maximumFractionDigits") ) {
+
+			// e.g. For 3.1 where maximumFractionDigits=1, a pattern-provided minimumFractionDigits=3 would have to be normalized to 1
+			minimumFractionDigits = maximumFractionDigits;
+		}
+
+	}
+
+	if ( isFinite(minimumSignificantDigits) && isFinite(maximumSignificantDigits) ) {
+
+		if ( minimumSignificantDigits < 1 || minimumSignificantDigits > 21 ) {
+			throw new Error( "Minimum significant digits is out of range" );
+		}
+		if ( minimumSignificantDigits < 1 || maximumSignificantDigits > 21 ) {
+			throw new Error( "Maximum significant digits is out of range" );
+		}
+
+		if ( minimumSignificantDigits > maximumSignificantDigits ) {
+
+			// Cases where minimum and maximum significant digits options are provided
+			if ( options.hasOwnProperty("minimumSignificantDigits") && options.hasOwnProperty("maximumSignificantDigits") ) {
+				throw new Error( "Minimum significant digits cannot be greater than its maximum" );
+
+			// e.g. For 123.00 where minimumSignificantDigits=6, a pattern-provided maximumSignificantDigits=3 would have to be normalized to 5
+			} else if ( options.hasOwnProperty("minimumSignificantDigits") ) {
+				maximumSignificantDigits = minimumSignificantDigits;
+
+			// e.g. For 120 where maximumSignificantDigits=2, a pattern-provided minimumSignificantDigits=3 would have to be normalized to 2
+			} else if ( options.hasOwnProperty("maximumSignificantDigits") ) {
+				minimumSignificantDigits = maximumSignificantDigits;
+			}
+		}
+
+	} else if ( isFinite(minimumSignificantDigits) || isFinite(maximumSignificantDigits) ) {
+		throw new Error( "None or both the minimum and maximum significant digits must be present" );
+	}
 
 	// Negative pattern
 	// "If there is an explicit negative subpattern, it serves only to specify the negative prefix and suffix" UTS#35
@@ -84,8 +158,6 @@ return function( number, pattern, cldr, options ) {
 	// Significant digit format
 	if ( minimumSignificantDigits && maximumSignificantDigits ) {
 		number = numberFormatSignificantDigits( number, minimumSignificantDigits, maximumSignificantDigits, round );
-	} else if ( minimumSignificantDigits || maximumSignificantDigits ) {
-		throw new Error( "None or both the minimum and maximum significant digits must be present" );
 
 	// Integer and fractional format
 	} else {
