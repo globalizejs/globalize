3cd0afa5c9e4563eb5b24e95b9b0cae0acedb8f9
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-11-26 09:46:56 -0200

    Currency: Create initial currency module
    Ref #238
    Ref #351

---
+++ src/currency.js
@@ -0,0 +1,67 @@
+define([
+	"./core",
+	"cldr/event"
+], function( Globalize ) {
+
+/**
+ * .currencyFormatter( currency [, options] )
+ *
+ * @currency [String] 3-letter currency code as defined by ISO 4217.
+ *
+ * @options [Object]:
+ * - form: [String] "symbol" (default), "code" or "name".
+ * - see also number/format options.
+ *
+ * Return a function that formats a currency according to the given options and default/instance
+ * locale.
+ */
+Globalize.currencyFormatter =
+Globalize.prototype.currencyFormatter = function( currency, options ) {
+};
+
+/**
+ * .currencyParser( currency [, options] )
+ *
+ * @currency [String] 3-letter currency code as defined by ISO 4217.
+ *
+ * @options [Object] see currencyFormatter.
+ *
+ * Return the currency parser according to the given options and the default/instance locale.
+ */
+Globalize.currencyParser =
+Globalize.prototype.currencyParser = function( currency, options ) {
+};
+
+/**
+ * .formatCurrency( value, currency [, options] )
+ *
+ * @value [Number] number to be formatted.
+ *
+ * @currency [String] 3-letter currency code as defined by ISO 4217.
+ *
+ * @options [Object] see currencyFormatter.
+ *
+ * Format a currency according to the given options and the default/instance locale.
+ */
+Globalize.formatCurrency =
+Globalize.prototype.formatCurrency = function( value, currency, options ) {
+};
+
+/**
+ * .parseCurrency( value, currency [, options] )
+ *
+ * @value [String]
+ *
+ * @currency [String] 3-letter currency code as defined by ISO 4217.
+ *
+ * @options [Object]: See currencyFormatter.
+ *
+ * Return the parsed currency or NaN when value is invalid.
+ */
+Globalize.parseCurrency =
+Globalize.prototype.parseCurrency = function( value, currency, options ) {
+};
+
+return Globalize;
+
+});
