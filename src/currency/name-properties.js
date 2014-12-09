18f4d020515393f9852508402f68da5d41498aa9
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-12-09 12:41:38 -0200

    (fix) Currency: Implement currency formatting (name and code mode)

--- src/currency/name-properties.js
+++ src/currency/name-properties.js
@@ -1,9 +1,11 @@
 define([
+	"./code-properties",
 	"./supplemental-override",
 	"./unit-patterns",
 	"../number/pattern",
 	"../util/object/filter"
-], function( currencySupplementalOverride, currencyUnitPatterns, numberPattern, objectFilter ) {
+], function( currencyCodeProperties, currencySupplementalOverride, currencyUnitPatterns,
+	numberPattern, objectFilter ) {
 
 /**
  * nameProperties( currency, cldr )
@@ -11,21 +13,14 @@ define([
  * Return number pattern with the appropriate currency code in as literal.
  */
 return function( currency, cldr ) {
-	var pattern = numberPattern( "decimal", cldr );
+	var properties = currencyCodeProperties( currency, cldr );
 
-	// The number of decimal places and the rounding for each currency is not locale-specific. Those
-	// values overridden by Supplemental Currency Data.
-	pattern = currencySupplementalOverride( currency, pattern, cldr );
+	properties.displayNames = objectFilter( cldr.main([
+		"numbers/currencies",
+		currency
+	]), /^displayName/ );
 
-	return {
-		currency: currency,
-		displayNames: objectFilter( cldr.main([
-			"numbers/currencies",
-			currency
-		]), /^displayName/ ),
-		pattern: pattern,
-		unitPatterns: currencyUnitPatterns( cldr )
-	};
+	return properties;
 };
 
 });
