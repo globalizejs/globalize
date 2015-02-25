1ec5882b5b9f745729f9c73c3149cbe93aeeaf1f
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-02-25 09:54:46 -0300

    Example: Use `require( "cldr-data/<path>" )`
    Use `require( "cldr-data/<path>" )` instead of `cldr( <path> )` on
    Node.js example.

--- examples/node-npm/main.js
+++ examples/node-npm/main.js
@@ -1,19 +1,18 @@
 #! /usr/bin/node
 
 var like;
-var cldrData = require( "cldr-data" );
 var Globalize = require( "globalize" );
 
 // Before we can use Globalize, we need to feed it on the appropriate I18n content (Unicode CLDR). Read Requirements on Getting Started on the root's README.md for more information.
 Globalize.load(
-	cldrData( "main/en/currencies" ),
-	cldrData( "main/en/ca-gregorian" ),
-	cldrData( "main/en/numbers" ),
-	cldrData( "supplemental/currencyData" ),
-	cldrData( "supplemental/likelySubtags" ),
-	cldrData( "supplemental/plurals" ),
-	cldrData( "supplemental/timeData" ),
-	cldrData( "supplemental/weekData" )
+	require( "cldr-data/main/en/currencies" ),
+	require( "cldr-data/main/en/ca-gregorian" ),
+	require( "cldr-data/main/en/numbers" ),
+	require( "cldr-data/supplemental/currencyData" ),
+	require( "cldr-data/supplemental/likelySubtags" ),
+	require( "cldr-data/supplemental/plurals" ),
+	require( "cldr-data/supplemental/timeData" ),
+	require( "cldr-data/supplemental/weekData" )
 );
 Globalize.loadMessages( require( "./messages/en" ) );
 
