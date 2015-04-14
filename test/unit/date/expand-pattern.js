68d6643d56ebf1f2218c9c1ff028f07c7e070215
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-04-14 09:03:22 -0300

    Test: Include unit test for expanding datetime skeletons
    Ref #272
    Ref #426

--- test/unit/date/expand-pattern.js
+++ test/unit/date/expand-pattern.js
@@ -1,43 +1,47 @@
 define([
 	"cldr",
 	"src/date/expand-pattern",
+	"json!cldr-data/main/de/ca-gregorian.json",
 	"json!cldr-data/main/en/ca-gregorian.json",
 	"json!cldr-data/supplemental/likelySubtags.json",
 
 	"cldr/event",
 	"cldr/supplemental"
-], function( Cldr, expandPattern, enCaGregorian, likelySubtags ) {
+], function( Cldr, expandPattern, deCaGregorian, enCaGregorian, likelySubtags ) {
 
-var cldr;
+var de, en;
 
-Cldr.load( enCaGregorian, likelySubtags );
+Cldr.load( deCaGregorian, enCaGregorian, likelySubtags );
 
-cldr = new Cldr( "en" );
+de = new Cldr( "de" );
+en = new Cldr( "en" );
 
 QUnit.module( "Date Expand Pattern" );
 
 QUnit.test( "should expand skeleton \"<skeleton>\" (as a String)", function( assert ) {
-	assert.equal( expandPattern( "GyMMMEd", cldr ), "E, MMM d, y G" );
+	assert.equal( expandPattern( "GyMMMEd", en ), "E, MMM d, y G" );
 });
 
 QUnit.test( "should expand {skeleton: \"<skeleton>\"}", function( assert ) {
-	assert.equal( expandPattern( { skeleton: "GyMMMEd" }, cldr ), "E, MMM d, y G" );
+	assert.equal( expandPattern( { skeleton: "GyMMMEd" }, en ), "E, MMM d, y G" );
+	assert.equal( expandPattern( { skeleton: "GyMMMEdhms" }, en ), "E, MMM d, y G, h:mm:ss a" );
+	assert.equal( expandPattern( { skeleton: "MMMMEdhm" }, de ), "E, d. MMMM 'um' h:mm a" );
 });
 
 QUnit.test( "should expand {date: \"(full, ...)\"}", function( assert ) {
-	assert.equal( expandPattern( { date: "full" }, cldr ), "EEEE, MMMM d, y" );
+	assert.equal( expandPattern( { date: "full" }, en ), "EEEE, MMMM d, y" );
 });
 
 QUnit.test( "should expand {time: \"(full, ...)\"}", function( assert ) {
-	assert.equal( expandPattern( { time: "full" }, cldr ), "h:mm:ss a zzzz" );
+	assert.equal( expandPattern( { time: "full" }, en ), "h:mm:ss a zzzz" );
 });
 
 QUnit.test( "should expand {datetime: \"(full, ...)\"}", function( assert ) {
-	assert.equal( expandPattern( { datetime: "full" }, cldr ), "EEEE, MMMM d, y 'at' h:mm:ss a zzzz" );
+	assert.equal( expandPattern( { datetime: "full" }, en ), "EEEE, MMMM d, y 'at' h:mm:ss a zzzz" );
 });
 
 QUnit.test( "should expand {pattern: \"<pattern>\"}", function( assert ) {
-	assert.equal( expandPattern( { pattern: "MMM d" }, cldr ), "MMM d" );
+	assert.equal( expandPattern( { pattern: "MMM d" }, en ), "MMM d" );
 });
 
 });
