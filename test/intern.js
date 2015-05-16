14cc50e0bdc7aa7f9a8d661476d929d34d918fc6
Amanpreet Singh <apsdehal@gmail.com>
2015-05-16 14:52:44 -0700

    Tests: Add intern's configs
      - Local
      - General

---
+++ test/intern.js
@@ -0,0 +1,34 @@
+define({
+  proxyPort: 9000,
+  proxyUrl: "http://localhost:9000/",
+  capabilities: {},
+  environments: [
+    { browserName: "chrome" },
+    { browserName: "internet explorer", version: [ "11", "10" ] },
+    { browserName: "firefox", version: "34" }
+  ],
+  maxConcurrency: 2,
+  tunnel: "BrowserStackTunnel",
+  useLoader: {
+    'host-node': 'requirejs',
+    'host-browser': '../../external/requirejs/require.js'
+  },
+  loader: {
+    paths: {
+      qunit: "../external/qunit/qunit/qunit",
+      cldr: "../external/cldrjs/dist/cldr",
+      "cldr-data": "../external/cldr-data",
+      globalize: "../dist/globalize",
+      json: "../external/requirejs-plugins/src/json",
+      src: "../src",
+      text: "../external/requirejs-text/text"
+    }
+  },
+  suites: [
+    "test/unit/all"
+  ],
+  functionalSuites: [
+    // "test/functional/all"
+  ],
+  excludeInstrumentation: /^(?:node_modules|test)\//
+});
