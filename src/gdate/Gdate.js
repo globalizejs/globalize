5abdd073283b883e2d0ae329a32832541ba7f386
Danny Wachsstock <d.wachss@prodigy.net>
2015-06-22 10:03:53 -0500

    GDate.js: fix whitespace

--- src/gdate/Gdate.js
+++ src/gdate/Gdate.js
@@ -9,58 +9,58 @@ define(function() {
 
 function Gdate(){}
 Gdate.prototype = {
-  getEra: function() { return this._era; },
-  getYear: function() { return this._year; },
-  getMonth: function() { return this._month; },
-  getDate: function() { return this._date; },
-  nextDate: function( n ) {
-    if (arguments.length === 0){
+	getEra: function() { return this._era; },
+	getYear: function() { return this._year; },
+	getMonth: function() { return this._month; },
+	getDate: function() { return this._date; },
+	nextDate: function( n ) {
+		if (arguments.length === 0){
 			n = 1;
 		}
 		// I'm getting errors with new Date(this._d)
 		var d = new Date(this._d.getTime());
-    d.setDate(this._d.getDate() + n);
-    return new this.constructor(d);
-  },
+		d.setDate(this._d.getDate() + n);
+		return new this.constructor(d);
+	},
 	nextYear: undefined, // virtual function
 	nextMonth: undefined, // virtual function
 	startOfMonth: function() {
 		return this.nextDate( 1 - this._date );
 	},
 	startOfYear: function() {
-    // no choice but to go through each month one at a time
+		// no choice but to go through each month one at a time
 		var thisMonth = this,
 			lastMonth = thisMonth.nextMonth(-1);
-   while ( lastMonth.getYear() === thisMonth._year ){
-      thisMonth = lastMonth;
+		while ( lastMonth.getYear() === thisMonth._year ){
+			thisMonth = lastMonth;
 			lastMonth = thisMonth.nextMonth(-1);
-    }
+		}
 		return thisMonth.startOfMonth();
-  },
-  toDate: function() {
+	},
+	toDate: function() {
 		// we need to make sure that an arbitrary time doesn't leak through
 		var d = new Date( this._d.getFullYear(), this._d.getMonth(), this._d.getDate(),
 			0, 0, 0, 0 );
 		d.setFullYear( this._d.getFullYear() ); // deal with Y2K bug in Javascript
 		return d;
 	},
-  _init: function( era, year, month, date ) {
-    if (era instanceof Date){
-      this._setDate(era);
-    }else if ( era instanceof Gdate ){
-      this._setFields (era.getEra(), era.getYear(), era.getMonth(), era.getDate());
-    }else {
-      this._setFields (era, year, month, date);
-    }
-  },
+	_init: function( era, year, month, date ) {
+		if (era instanceof Date){
+			this._setDate(era);
+		}else if ( era instanceof Gdate ){
+			this._setFields (era.getEra(), era.getYear(), era.getMonth(), era.getDate());
+		}else {
+			this._setFields (era, year, month, date);
+		}
+	},
 	constructor: Gdate, // allow the new this.constructor idiom
 	_setDate: undefined, // virtual function
 	_setFields: undefined, // virtual function
-  _era: NaN,
-  _year: NaN,
-  _month: undefined,
-  _date: NaN,
-  _d: new Date(NaN)
+	_era: NaN,
+	_year: NaN,
+	_month: undefined,
+	_date: NaN,
+	_d: new Date(NaN)
 };
 
 Gdate.calendars = {}; // this will store the calendar algorithms (the Gdate subclass constructors)
