a16e8309eb9600b923bbecf34cd43febe95ee275
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-08-11 09:32:15 -0300

    fixup! All: Runtime support (smaller and quicker code for production)
    Why is this needed?

--- src/common/runtime-key.js
+++ src/common/runtime-key.js
@@ -4,7 +4,6 @@ define([
 
 return function( fnName, locale, args ) {
 	var hash;
-	args = [].slice.call( args, 0 );
 	hash = stringHash( fnName + locale + JSON.stringify( args ) );
 	return hash > 0 ? "a" + hash : "b" + Math.abs( hash );
 };
--- src/message-runtime.js
+++ src/message-runtime.js
@@ -11,7 +11,7 @@ Globalize._validateParameterTypeMessageVariables = validateParameterTypeMessageV
 
 Globalize.messageFormatter =
 Globalize.prototype.messageFormatter = function( /* path */ ) {
-	return Globalize[ runtimeKey( "messageFormatter", this._locale, arguments ) ];
+	return Globalize[ runtimeKey( "messageFormatter", this._locale, [].slice.call( arguments, 0 ) ) ];
 };
 
 Globalize.formatMessage =
