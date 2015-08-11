a03a92db431a0d2c9e42ed3b24c8654ce490e200
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-08-11 09:00:00 -0300

    fixup! All: Runtime support (smaller and quicker code for production)
    Can't you just access fn.name?

--- src/common/runtime-bind.js
+++ src/common/runtime-bind.js
@@ -3,16 +3,15 @@ define([
 ], function( runtimeKey ) {
 
 return function( args, cldr, fn, runtimeArgs ) {
-	var fnName = /^function\s+([\w\$]+)\s*\(/.exec( fn.toString() )[ 1 ],
-		locale = cldr.locale;
+	var locale = cldr.locale;
 
-	fn.runtimeKey = runtimeKey( fnName, locale, args );
+	fn.runtimeKey = runtimeKey( fn.name, locale, args );
 
 	// Stringify arguments.
 	args = JSON.stringify( args ).slice( 1, -1 );
 
 	fn.generatorString = function() {
-		return "Globalize(\"" + locale + "\")." + fnName + "(" + args + ")";
+		return "Globalize(\"" + locale + "\")." + fn.name + "(" + args + ")";
 	};
 
 	fn.runtimeArgs = runtimeArgs;
