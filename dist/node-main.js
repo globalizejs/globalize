0cc2f8a5b3303e1f6fca1be29d9c964e599549e0
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-10-02 13:26:41 -0300

    Build: Include distribution files

---
+++ dist/node-main.js
@@ -0,0 +1,26 @@
+/*!
+ * Globalize v1.1.0-rc.3
+ *
+ * http://github.com/jquery/globalize
+ *
+ * Copyright jQuery Foundation and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2015-10-02T16:26Z
+ */
+
+// Core
+module.exports = require( "./globalize" );
+
+// Extent core with the following modules
+require( "./globalize/message" );
+require( "./globalize/number" );
+require( "./globalize/plural" );
+
+// Load after globalize/number
+require( "./globalize/currency" );
+require( "./globalize/date" );
+
+// Load after globalize/number and globalize/plural
+require( "./globalize/relative-time" );
