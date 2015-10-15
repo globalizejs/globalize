bebb6419897f0947c81d317d7eaa5bb7e0ad4c22
Amanpreet Singh <apsdehal@gmail.com>
2015-10-15 18:25:05 +0530

    Tests: Update intern config for browsers

--- test/intern.js
+++ test/intern.js
@@ -1,40 +1,51 @@
 
 define({
-  proxyPort: 9000,
-  proxyUrl: "http://localhost:9000/",
-  capabilities: {},
-  environments: [
-    { browserName: "safari", version: [ "6" ], platform: [ "Mac 10.8" ] },
-    { browserName: "chrome", version: [ "45", "44" ] },
-    { browserName: "internet explorer", version: [ "11", "10", "9" ] },
-    { browserName: "firefox", version: [ "41", "40" ] },
-    { browserName: "opera", version: [ "12.16", "12.15" ] },
-    { browserName: "iPad", platform: [ "ios" ] },
-    { browserName: "iPhone", platform: [ "ios" ] },
-    { browserName: "Android", platform: [ "android" ] }
-  ],
-  maxConcurrency: 2,
-  tunnel: "BrowserStackTunnel",
-  loaders: {
-    "host-node": "requirejs",
-    "host-browser": "external/requirejs/require.js"
-  },
-  loaderOptions: {
-    waitSeconds: 8,
-    paths: {
-      qunit: "external/qunit/qunit/qunit",
-      cldr: "external/cldrjs/dist/cldr",
-      "cldr-data": "external/cldr-data",
-      globalize: "dist/globalize",
-      json: "external/requirejs-plugins/src/json",
-      src: "src",
-      text: "external/requirejs-text/text"
-    }
-  },
-  suites: [
-    "test/unit/all",
-    "test/functional/all"
-  ],
-  unitTestsWaitForReporter: false,
-  excludeInstrumentation: /^(?:node_modules|test)\//
+	proxyPort: 9000,
+	proxyUrl: "http://localhost:9000/",
+	capabilities: {},
+	environments: [
+		{ browserName: "internet explorer", version: "11", platform: "Win8",
+			name : "globalize"},
+		{ browserName: "internet explorer", version: "10", platform: "Windows",
+			name : "globalize"},
+		{ browserName: "internet explorer", version: "9", platform: "Windows" },
+		{ browserName: "firefox", version: "41", platform: ["Windows" ], name : "globalize"}
+		{ browserName: "firefox", version: "40", platform: ["Windows" ], name : "globalize"}
+		{ browserName: "chrome", version: "45", platform: [ "Windows" ],
+			name : "globalize"},
+		{ browserName: "chrome", version: "44", platform: [ "Windows" ],
+			name : "globalize"},
+		{ browserName: "safari", version: "7", platform: [ "mac" ], name : "globalize"},
+		{ browserName: "iphone 7.1 simulator", platform: "mac", version: "7.1", deviceName: "iPhone",
+			app: "safari", device: "iPhone Simulator", name: "globalize" }
+
+		{ browserName: "android", platform: "Linux", version: "4.1", name : "globalize"},	
+	],
+	maxConcurrency: 3,
+	tunnel: "BrowserStackTunnel",
+	loaders: {
+		"host-node": "requirejs",
+		"host-browser": "external/requirejs/require.js"
+	},
+	loaderOptions: {
+		waitSeconds: 8,
+		paths: {
+			qunit: "external/qunit/qunit/qunit",
+			cldr: "external/cldrjs/dist/cldr",
+			"cldr-data": "external/cldr-data",
+			globalize: "dist/globalize",
+			json: "external/requirejs-plugins/src/json",
+			src: "src",
+			text: "external/requirejs-text/text"
+		}
+	},
+	POLL_INTERVAL: 500, // milliseconds
+	defaultTimeout: 300000, // 5 minutes
+	WAIT_TIMEOUT: 180000, // 3 minutes
+	suites: [
+		"test/unit/all",
+		"test/functional/all"
+	],
+	unitTestsWaitForReporter: false,
+	excludeInstrumentation: /^(?:node_modules|test)\//
 });
