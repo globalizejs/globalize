aa13fbe7dd020a12fad5281d097c84bbc8ef6d55
Amanpreet Singh <apsdehal@gmail.com>
2015-10-15 18:59:24 +0530

    Tests: Fix jshint errors

--- test/intern.js
+++ test/intern.js
@@ -5,21 +5,20 @@ define({
 	capabilities: {},
 	environments: [
 		{ browserName: "internet explorer", version: "11", platform: "Win8",
-			name : "globalize"},
+			name : "globalize" },
 		{ browserName: "internet explorer", version: "10", platform: "Windows",
-			name : "globalize"},
+			name : "globalize" },
 		{ browserName: "internet explorer", version: "9", platform: "Windows" },
-		{ browserName: "firefox", version: "41", platform: ["Windows" ], name : "globalize"}
-		{ browserName: "firefox", version: "40", platform: ["Windows" ], name : "globalize"}
+		{ browserName: "firefox", version: [ "41", "40" ], platform: [ "Windows" ], name: "globalize" },
 		{ browserName: "chrome", version: "45", platform: [ "Windows" ],
-			name : "globalize"},
+			name : "globalize" },
 		{ browserName: "chrome", version: "44", platform: [ "Windows" ],
-			name : "globalize"},
-		{ browserName: "safari", version: "7", platform: [ "mac" ], name : "globalize"},
-		{ browserName: "iphone 7.1 simulator", platform: "mac", version: "7.1", deviceName: "iPhone",
-			app: "safari", device: "iPhone Simulator", name: "globalize" }
+			name : "globalize" },
+		{ browserName: "safari", version: "7", platform: [ "mac" ], name : "globalize" },
+		{ browserName: "iphone 7.1 simulator", platform: "mac", version: "7.1", 
+			deviceName: "iPhone", app: "safari", device: "iPhone Simulator", name: "globalize" },
 
-		{ browserName: "android", platform: "Linux", version: "4.1", name : "globalize"},	
+		{ browserName: "android", platform: "Linux", version: "4.1", name : "globalize" }
 	],
 	maxConcurrency: 3,
 	tunnel: "BrowserStackTunnel",
