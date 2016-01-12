f10a690493071be65906ac5cab937c28235c0753
Danny Wachsstock <d.wachss@prodigy.net>
2016-01-12 12:06:21 -0600

    astronomy: implement newMoon

--- src/gdate/astronomy.js
+++ src/gdate/astronomy.js
@@ -9,11 +9,12 @@ var EPSILON = 60000, // one minute accuracy
 	DAY_MS = 86400000, // ms in a day
 	JD_EPOCH_MS = -210866760000000, // time of the julian day epoch, January 1, 4713 BCE
 	JD_SUN_EPOCH = 2447891.5, // Julian day of the calculation epoch (December 31, 1989)
-    MOON_L0 = 318.351648 * Math.PI / 180,   // Mean long. at epoch
-    MOON_P0 =  36.340410 * Math.PI / 180,  // Mean long. of perigee
-    MOON_N0 = 318.510107 * Math.PI / 180,   // Mean long. of node
-    MOON_I  =   5.145366 * Math.PI / 180,   // Inclination of orbit
-    MOON_E  =   0.054900,            // Eccentricity of orbit
+	MOON_L0 = 318.351648 * Math.PI / 180,   // Mean long. at epoch
+	MOON_P0 =  36.340410 * Math.PI / 180,  // Mean long. of perigee
+	MOON_N0 = 318.510107 * Math.PI / 180,   // Mean long. of node
+	MOON_I  =   5.145366 * Math.PI / 180,   // Inclination of orbit
+	MOON_E  =   0.054900,            // Eccentricity of orbit
+	DEG2RAD = Math.PI / 180, // convert degrees to radians
 	SUN_ETA_G = 4.89078, // Ecliptic longitude at epoch, from
 	// https://www.nrel.gov/midc/solpos/spa.html for 1970-01-01 at 00:00
 	SUN_OMEGA_G = 282.9372 * Math.PI / 180, // Ecliptic longitude at perigee
@@ -82,6 +83,33 @@ function trueSunAnomaly( time ){
 		5.05374684623142E-006 * Math.sin( 3 * meanAnomaly );
 }
 
+/* returns the angle between the ecliptic longitude of the sun and the moon */
+/* see the ICU CalendarAstronomer method getMoonPosition */
+/* unfortunately, their source used degrees for the constants, 
+   so there's a lot of conversions here */
+function moonAge( time ){
+	sunLong = sunLongitude ( time );
+	meanAnomalySun = meanSunAnomaly( time );
+	day = ms2jd ( time ) - JD_SUN_EPOCH; // days since the calculation epoch
+	meanLongitude = normTAU( day * 13.1763966 * DEG2RAD + MOON_L0 );
+	meanAnomalyMoon = normTAU( meanLongitude - day * 0.1114041 * DEG2RAD - MOON_P0 );
+	evection = 1.2739 * DEG2RAD * Math.sin( 2 * (meanLongitude - sunLong ) - meanAnomalyMoon );
+	annual = 0.1858 * DEG2RAD * Math.sin( meanAnomalySun );
+	meanAnomalyMoon += evection - annual - 0.3700 * DEG2RAD * Math.sin( meanAnomalySun );
+	center = 6.2886 * DEG2RAD * Math.sin( meanAnomalyMoon ) +
+		0.2140 * DEG2RAD * Math.sin ( 2 * meanAnomalyMoon );
+	moonLongitude = meanLongitude + evection + center - annual;
+	variation = 0.6583 * DEG2RAD * Math.sin( 2 * ( moonLongitude - sunLong ) );
+	moonLongitude += variation;
+	// the ascending node is the point where the ecliptic crosses the moon's orbit
+	nodeLongitude = normTAU ( MOON_N0 - day * 0.0529529 * DEG2RAD ) -
+		0.16 * DEG2RAD * Math.sin( meanAnomalySun );
+	x = Math.cos ( moonLongitude - nodeLongitude );
+	y = Math.sin ( moonLongitude - nodeLongitude );
+	moonEclipLong = Math.atan2( y * Math.cos( MOON_I ), x) + nodeLongitude;
+	return normTAU( moonEclipLong - sunLong)
+}
+
 /* given a function func( time ) that returns an angle, uses interpolation
  * to return the time when the function
  * will have the desired angle, either after (next === true) or
@@ -125,19 +153,16 @@ function closeEnoughAngle( f1, f2 ) {
 	return Math.abs( f1 - f2 ) < 1e-5;
 }
 
+/* not used currently 
 function normPI( angle ) {
 	// constrain to [-PI..PI)
 	return normTAU( angle + Math.PI) - Math.PI;
 }
+*/
+
 function normTAU( angle ) {
 	// constrain to [0..TAU)
 	return angle - TAU * Math.floor( angle / TAU );
 }
 
-function foo ( angle ) {
-	// junk function to fool lint
-	return normPI( angle ) + JD_SUN_EPOCH + MOON_E + MOON_I + MOON_L0 + MOON_N0 +
-		MOON_P0;
-}
-Gdate.foo = foo;
 });
