ae4fa6c3c17825c7e08908c834d665cf1738d0d3
Rafael Xavier de Souza <rxaviers@gmail.com>
2016-01-17 10:41:04 -0200

    Runtime: Minor change, include comment
    Ref #573

--- src/common/runtime-bind.js
+++ src/common/runtime-bind.js
@@ -3,8 +3,10 @@ define([
 ], function( runtimeKey ) {
 
 return function( args, cldr, fn, runtimeArgs ) {
+
+	// 1: fn.name isn't supported by IE.
 	var argsStr = JSON.stringify( args ),
-		fnName = fn.name || /^function\s+([\w\$]+)\s*\(/.exec( fn.toString() )[ 1 ],
+		fnName = fn.name || /^function\s+([\w\$]+)\s*\(/.exec( fn.toString() )[ 1 ], /* 1 */
 		locale = cldr.locale;
 
 	fn.runtimeKey = runtimeKey( fnName, locale, null, argsStr );
