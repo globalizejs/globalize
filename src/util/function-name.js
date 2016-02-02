15103d424bab6d94add4be03db0c372d2c109b16
Pavel Karoukin <pavel@karoukin.us>
2016-02-01 19:52:07 -0600

    Core: Let's keep track of // fn.name is not supported by IE

--- src/util/function-name.js
+++ src/util/function-name.js
@@ -5,6 +5,7 @@ return function( fn ) {
 		return fn.name;
 	}
 
+	// fn.name is not supported by IE
 	var matches = /^function\s+([\w\$]+)\s*\(/.exec( fn.toString() );
 
 	if ( matches && matches.length > 0 ) {
