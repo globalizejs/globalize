9db5ea0b78f3b509db1d72436944cfb586dbe92a
Artur Eshenbrener <strate@yandex.ru>
2016-04-03 15:37:06 +0300

    DateTimeFormatter: augment skeleton refactoring
    Rework of `getBestMatchPattern` algorithm
     - properly handle close parts of sekletons, i.e. `L` and `M`
     - better distance calculating
    Call to `getBestMatchPattern` before splitting skeleton to date and time parts
    Add a lot of test cases to `expandPattern` function.
    
    Fixes #271

--- test/unit/date/expand-pattern.js
+++ test/unit/date/expand-pattern.js
@@ -3,25 +3,83 @@ define([
 	"src/date/expand-pattern",
 	"json!cldr-data/main/de/ca-gregorian.json",
 	"json!cldr-data/main/en/ca-gregorian.json",
+	"json!cldr-data/main/ru/ca-gregorian.json",
 	"json!cldr-data/supplemental/likelySubtags.json",
 
 	"cldr/event",
 	"cldr/supplemental"
-], function( Cldr, expandPattern, deCaGregorian, enCaGregorian, likelySubtags ) {
+], function( Cldr, expandPattern, deCaGregorian, enCaGregorian, ruCaGregorian, likelySubtags ) {
 
-var de, en;
+var de, en, ru;
 
-Cldr.load( deCaGregorian, enCaGregorian, likelySubtags );
+Cldr.load( deCaGregorian, enCaGregorian, ruCaGregorian, likelySubtags );
 
 de = new Cldr( "de" );
 en = new Cldr( "en" );
+ru = new Cldr( "ru" );
+
+/**
+ * Test actual patterns here
+ * @see https://ssl.icu-project.org/icu4jweb/flexTest.jsp
+ */
 
 QUnit.module( "Date Expand Pattern" );
 
+
 QUnit.test( "should expand {skeleton: \"<skeleton>\"}", function( assert ) {
-	assert.equal( expandPattern( { skeleton: "GyMMMEd" }, en ), "E, MMM d, y G" );
-	assert.equal( expandPattern( { skeleton: "GyMMMEdhms" }, en ), "E, MMM d, y G, h:mm:ss a" );
-	assert.equal( expandPattern( { skeleton: "MMMMEdhm" }, de ), "E, d. MMMM 'um' h:mm a" );
+	var cldrs = {
+		en: en,
+		de: de,
+		ru: ru
+	};
+	var cases = {
+		en: {
+			"GyMMMEd": "E, MMM d, y G",
+			"GyMMMEdhms": "E, MMM d, y G, h:mm:ss a",
+			"MMMMEdhm": "E, MMMM d 'at' h:mm a",
+			"hhmm": "hh:mm a",
+			"HHmm": "HH:mm",
+			"EHmss": "E HH:mm:ss",
+			"MMMMh": "LLLL, h a"
+		},
+		de: {
+			"yMMMMd": "d. MMMM y",
+			"MMMMd": "d. MMMM",
+			"MMMM": "LLLL",
+			"MMMMy": "MMMM y",
+			"EEEE": "cccc",
+			"cccc": "cccc",
+			"EEEEMMMMd": "EEEE, d. MMMM",
+			"ccccMMMMd": "EEEE, d. MMMM",
+			"HHmm": "HH:mm",
+			"EEEEHHmm": "EEEE, HH:mm",
+			"EEEEHmm": "EEEE, HH:mm",
+			"ccccHmm": "EEEE, HH:mm",
+			"MMMMEdhm": "E, d. MMMM 'um' h:mm a"
+		},
+		ru: {
+			"yMMMMd": "d MMMM y 'г'.",
+			"MMMMd": "d MMMM",
+			"MMMM": "LLLL",
+			"MMMMy": "LLLL y 'г'.",
+			"EEEE": "cccc",
+			"cccc": "cccc",
+			"EEEEMMMMd": "cccc, d MMMM",
+			"ccccMMMMd": "cccc, d MMMM",
+			"HHmm": "HH:mm",
+			"EEEEHHmm": "EEEE HH:mm",
+			"EEEEHmm": "EEEE HH:mm",
+			"ccccHHmm": "EEEE HH:mm",
+			"ccccHmm": "EEEE HH:mm",
+			"MMMMEdhm": "ccc, d MMMM, h:mm a"
+		}
+	};
+	Object.keys( cases ).forEach( function( locale ) {
+		Object.keys( cases[locale] ).forEach( function( skeleton ) {
+			var expected = cases[locale][skeleton];
+			assert.equal( expandPattern( {skeleton: skeleton}, cldrs[locale] ), expected, locale + ", " + skeleton );
+		} );
+	} );
 });
 
 QUnit.test( "should expand {date: \"(full, ...)\"}", function( assert ) {
@@ -38,13 +96,6 @@ QUnit.test( "should expand {datetime: \"(full, ...)\"}", function( assert ) {
 
 QUnit.test( "should expand {raw: \"<pattern>\"}", function( assert ) {
 	assert.equal( expandPattern( { raw: "MMM d" }, en ), "MMM d" );
-});
-
-QUnit.test( "should augment {skeleton: \"<skeleton>\"}", function( assert ) {
-	assert.equal( expandPattern( { skeleton: "yMMMMd" }, de ), "d. MMMM y" );
-	assert.equal( expandPattern( { skeleton: "MMMMd" }, de ), "d. MMMM" );
-	assert.equal( expandPattern( { skeleton: "MMMM" }, de ), "MMMM" );
-	assert.equal( expandPattern( { skeleton: "EEEE" }, de ), "EEEE" );
 });
 
 });
