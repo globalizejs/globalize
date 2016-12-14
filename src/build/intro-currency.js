86c29b39ae5e1bc686e79b5ea514eb9636859f42
Rafael Xavier de Souza <rxaviers@gmail.com>
2016-12-14 16:32:21 -0200

    Revert "Revert "Build: Use relative path in inner modules""
    This reverts commit 4e9a2f882ddf8521ac407207dce04ac348281270.

--- src/build/intro-currency.js
+++ src/build/intro-currency.js
@@ -25,7 +25,7 @@
 	} else if ( typeof exports === "object" ) {
 
 		// Node, CommonJS
-		module.exports = factory( require( "cldrjs" ), require( "globalize" ) );
+		module.exports = factory( require( "cldrjs" ), require( "../globalize" ) );
 	} else {
 
 		// Global
--- src/build/intro-date.js
+++ src/build/intro-date.js
@@ -29,7 +29,7 @@
 	} else if ( typeof exports === "object" ) {
 
 		// Node, CommonJS
-		module.exports = factory( require( "cldrjs" ), require( "globalize" ) );
+		module.exports = factory( require( "cldrjs" ), require( "../globalize" ) );
 	} else {
 
 		// Extend global
