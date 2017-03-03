6118055a980b16f14205e69ce82b6aa870ea8c59
Rafael Xavier de Souza <rxaviers@gmail.com>
2017-03-03 19:14:53 -0300

    Date: Add formatDateToParts (2/2)

--- src/date/fields-map.js
+++ src/date/fields-map.js
@@ -1,14 +1,26 @@
 define([
-	"./inverse-fields-map"
-], function( dateInverseFieldsMap ) {
+	"../util/object/invert"
+], function( objectInvert ) {
 
 // Invert key and values, e.g., {"year": "yY"} ==> {"y": "year", "Y": "year"}
-return Object.keys( dateInverseFieldsMap ).reduce( function( map, key ) {
-	var value = dateInverseFieldsMap[ key ];
+return objectInvert({
+	"era": "G",
+	"year": "yY",
+	"quarter": "qQ",
+	"month": "ML",
+	"week": "wW",
+	"day": "dDF",
+	"weekday": "ecE",
+	"dayperiod": "a",
+	"hour": "hHkK",
+	"minute": "m",
+	"second": "sSA",
+	"zone": "zOxX"
+}, function( object, key, value ) {
 	value.split( "" ).forEach(function( symbol ) {
-		map[ symbol ] = key;
+		object[ symbol ] = key;
 	});
-	return map;
-}, {});
+	return object;
+});
 
 });
