c85af7c406a6df0f7b1e6315987ba72298f9c04f
Nikola Kovacs <nikola.kovacs@gmail.com>
2017-05-11 16:05:37 +0200

    Runtime: Fix unitFormatter with numberFormatter
    JSON.stringify omits functions, so the generated runtimeKey
    did not depend on the value of the numberFormatter option,
    causing different unitFormatters to have an identical runtimeKey.
    
    Fixes #704

--- src/common/runtime-key.js
+++ src/common/runtime-key.js
@@ -1,10 +1,11 @@
 define([
+	"./runtime-stringify",
 	"../util/string/hash"
-], function( stringHash ) {
+], function( runtimeStringify, stringHash ) {
 
 return function( fnName, locale, args, argsStr ) {
 	var hash;
-	argsStr = argsStr || JSON.stringify( args );
+	argsStr = argsStr || runtimeStringify( args );
 	hash = stringHash( fnName + locale + argsStr );
 	return hash > 0 ? "a" + hash : "b" + Math.abs( hash );
 };
