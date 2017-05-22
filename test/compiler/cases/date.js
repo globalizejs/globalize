111d11d680c22f1075354f9f1b780c8c05800dca
Nikola Kovacs <nikola.kovacs@gmail.com>
2017-05-22 15:39:50 +0200

    Date: add compiler test for timeZone

--- test/compiler/cases/date.js
+++ test/compiler/cases/date.js
@@ -8,6 +8,7 @@ module.exports = {
 			// date
 			require( "../../../external/cldr-data/main/en/ca-gregorian.json" ),
 			require( "../../../external/cldr-data/main/en/timeZoneNames.json" ),
+			require( "../../../external/cldr-data/supplemental/metaZones.json" ),
 			require( "../../../external/cldr-data/supplemental/timeData.json" ),
 			require( "../../../external/cldr-data/supplemental/weekData.json" ),
 			// number
@@ -15,12 +16,17 @@ module.exports = {
 			require( "../../../external/cldr-data/supplemental/numberingSystems.json" )
 		);
 
+		Globalize.loadTimeZone( require( "iana-tz-data" ) );
+
 		return Globalize;
 	},
 	cases: function( Globalize ) {
 		var date = new Date( 2010, 8, 15, 17, 35, 7, 369 );
 		Globalize.locale( "en" );
 		return [
+			{ formatter: Globalize.dateFormatter({ datetime: "full", timeZone: "Europe/Berlin" }), args: [ date ] },
+			{ formatter: Globalize.dateFormatter({ datetime: "full", timeZone: "America/Los_Angeles" }), args: [ date ] },
+
 			{ formatter: Globalize.dateFormatter({ skeleton: "GyMMMEd" }), args: [ date ] },
 			{ formatter: Globalize.dateFormatter({ skeleton: "dhms" }), args: [ date ] },
 			{ formatter: Globalize.dateFormatter({ skeleton: "GyMMMEdhms" }), args: [ date ] },
